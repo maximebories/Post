2737f25b8fde7f28490171a7187fb840df0f35f5
Maxime Bories <30550722+maximebories@users.noreply.github.com>
2021-10-21 07:46:17 +0200

    Refining Initial Setup
    Refining initial setup (Homebrew & its packages, mas  & static binaries .App, Security), Testings icons replacement (not for /System/Applications), Testing "re-nativefier" (Gmail, Inter, ...) with each its own icon & CSS. All this is still very messy & haven't look at dotfiles yet.

--- setup.sh
+++ setup.sh
@@ -1,4 +1,31 @@
-#!/bin/bash
+#!/bin/sh
+# Unatended configuration of post macOS installation
 
-chmod +x cli.sh
-./cli.sh &1> cli.sh.log
+# Anticipating...
+xcode-select --install
+
+# Homebrew installation, choose master or HEAD
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Add Homebrerw binaries to $PATH
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/maxime/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
+# Meh
+brew update
+brew upgrade
+
+# Tap, Tap, Tap dis bundle and install packages (mas packages not working atm, good old copy/paste /Applications)
+brew bundle
+
+# Adguard-home (Will be running somewhere else soon)
+
+curl -s -S -L https://raw.githubusercontent.com/AdguardTeam/AdGuardHome/master/scripts/install.sh | sh -s -- -v
+
+# UI-UX (only 1 example for now...)
+
+/Applications % fileicon set /Applications/GitHub\ Desktop.app /Users/maxime/Library/Mobile\ Documents/com~apple~CloudDocs/Scripts/Post-Install/icns/Github.icns
+
+# Oh-My-ZSH
+
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
