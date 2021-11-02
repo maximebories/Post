4a394dbf59a10b89d670a89455a32813a53368b9
Maxime Bories <maxime.bories@gmail.com>
2021-11-02 19:14:41 +0100

    PWA, Non-System icons done.

--- setup.sh
+++ setup.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
+
 # Unatended configuration of post macOS installation
 
-# Anticipating...
-xcode-select --install
+##################################################################
+#               Base, Pakage Management & Security               #
+##################################################################
 
-# Homebrew installation, choose master or HEAD
+# Homebrew installation, choose master or HEAD depending on context
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # Add Homebrerw binaries to $PATH
@@ -12,19 +14,19 @@ echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/maxime/.zprofile
 eval "$(/opt/homebrew/bin/brew shellenv)"
 
 # Meh
-brew update
-brew upgrade
+brew update && brew upgrade && brew cleanup && brew doctor
 
-# Tap, Tap, Tap dis bundle and install packages (mas packages not working atm, good old copy/paste /Applications)
+# Tap, Tap, Tap dis bundle and install dat packages (mas login not working atm,insert a breakpoint before)
 brew bundle
+brew reinstall --no-quarantine chromium
 
-# Adguard-home (Will be running somewhere else soon)
+# Adguard-home (Will be running somewhere else someday)
 
 curl -s -S -L https://raw.githubusercontent.com/AdguardTeam/AdGuardHome/master/scripts/install.sh | sh -s -- -v
 
-# UI-UX (only 1 example for now...)
+# UI-UX
 
-/Applications % fileicon set /Applications/GitHub\ Desktop.app /Users/maxime/Library/Mobile\ Documents/com~apple~CloudDocs/Scripts/Post-Install/icns/Github.icns
+ls ./icns/png | awk -F. '{print $1}' | xargs -I % sudo fileicon set /Applications/%.app ./icns/png/%.png
 
 # Oh-My-ZSH
 
